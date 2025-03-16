74ccf2f2b3f07f55ca0d993971b72397575a05e1
ananthks <ks.ananth1987@gmail.com>
2025-03-16 13:39:08 +0100

    Add deploy.yml

--- composeApp/src/commonMain/kotlin/com/whatsnew/composables/PreviewScreen.kt
+++ composeApp/src/commonMain/kotlin/com/whatsnew/composables/PreviewScreen.kt
@@ -1,18 +1,21 @@
 package com.whatsnew.composables
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
@@ -25,13 +28,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import coil3.compose.AsyncImage
 import kotlinx.coroutines.delay
 
 
-const val imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Tracy_E_Caldwell_portrait.jpg/1920px-Tracy_E_Caldwell_portrait.jpg"
+const val imageUrl =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Tracy_E_Caldwell_portrait.jpg/1920px-Tracy_E_Caldwell_portrait.jpg"
 
 @Composable
 fun PreviewScreen(onNavigate: () -> Unit) {
@@ -59,14 +65,14 @@ fun PreviewScreen(onNavigate: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp) // Add padding to avoid overlapping with the screen edges
+            .padding(16.dp)
     ) {
         FloatingActionButton(
-            onClick = { onNavigate() }, // Navigate to the next screen
-            text = "Next", // Customize the button text
+            onClick = { onNavigate() },
+            text = "Next",
             modifier = Modifier
-                .align(Alignment.BottomEnd) // Position the FAB in the bottom-right corner
-                .zIndex(1f) // Ensure the FAB is on top of other content
+                .align(Alignment.BottomEnd)
+                .zIndex(1f)
         )
     }
 }
@@ -75,15 +81,28 @@ fun PreviewScreen(onNavigate: () -> Unit) {
 @Composable
 fun ImageGallery() {
     val platforms = listOf("Android", "iOS")
-    val brands = listOf("FR", "KN", "HB")
+    val brands = listOf("FORTIS", "FINTRO", "HELLOBANK")
     val languages = listOf("EN", "FR", "NL", "DE")
 
     var selectedPlatform by remember { mutableStateOf(platforms[0]) }
-
-    Column {
-        // Platform Tabs
-        TabRow(selectedTabIndex = platforms.indexOf(selectedPlatform)) {
-            platforms.forEachIndexed { index, platform ->
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier.verticalScroll(scrollState)
+    ) {
+        Text(
+            "WhatsNew 1",
+            modifier = Modifier.background(Color.Gray).padding(12.dp),
+            style = MaterialTheme.typography.body1
+        )
+        Divider(modifier = Modifier.fillMaxWidth(), color = Color.Gray)
+        TabRow(
+            selectedTabIndex = platforms.indexOf(selectedPlatform),
+            backgroundColor = Color.Transparent,
+            modifier = Modifier
+                .width(200.dp)
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        ) {
+            platforms.forEachIndexed { _, platform ->
                 Tab(
                     selected = selectedPlatform == platform,
                     onClick = { selectedPlatform = platform },
@@ -92,42 +111,52 @@ fun ImageGallery() {
             }
         }
 
-        LazyColumn {
-            items(brands.size) { index: Int ->
-                Text(text = brands[index], style = MaterialTheme.typography.subtitle1, modifier = Modifier.padding(8.dp))
 
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(2),
-                    modifier = Modifier.fillMaxWidth().height(300.dp)
-                ) {
-                    items(languages) { language ->
-                        ImageCard(platform = selectedPlatform, brand = brands[index], language = language)
+        brands.forEachIndexed { _, item ->
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = item,
+                    style = MaterialTheme.typography.caption,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .width(150.dp)
+                        .wrapContentWidth(Alignment.CenterHorizontally),
+                    textAlign = TextAlign.Center
+                )
+
+                languages.forEach {
+                    Row {
+                        ImageCard(language = it)
                     }
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun ImageCard(platform: String, brand: String, language: String) {
+fun ImageCard(language: String) {
 
     Card(
-        modifier = Modifier.padding(8.dp).fillMaxWidth(),
+        modifier = Modifier.padding(8.dp),
     ) {
-        Column(modifier = Modifier.padding(8.dp)) {
-     /*       Image(
-                painter = painterResource(Res.drawable.compose_multiplatform),
-                contentDescription = "Brand Image",
-                modifier = Modifier.fillMaxWidth().height(120.dp)
-            )*/
+        Column(
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
             AsyncImage(
-                modifier = Modifier.size(240.dp),
+                modifier = Modifier.size(150.dp),
                 model = imageUrl,
                 contentDescription = "Brand Image"
             )
 
-            Text(text = "$brand - $language", modifier = Modifier.padding(8.dp))
+            Text(text = language, modifier = Modifier.padding(8.dp), style = MaterialTheme.typography.caption)
         }
     }
 }
